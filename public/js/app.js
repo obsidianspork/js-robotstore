7580b94e395dbae5f879ad886d79a09b2b81a0a5
David Coy <me@david-coy.com>
2017-11-29 20:08:10 -0600

    Integrate jQuery

---
+++ public/js/app.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  let $this = $(this);
+
+  // Highlight form inputs
+  $("form input").on("focusin", function() {
+    $this
+      .closest("p")
+      .addClass("active");
+  });
+  $("form input").on("focusout", function() {
+    $this
+      .closest("p")
+      .removeClass("active");
+  });
+
+  // Highlight Robots and Hardware on hover
+  $("fieldset li").on('mouseenter', function() {
+    $this.addClass("active");
+  });
+  $("fieldset li").on('mouseleave', function() {
+    $this.removeClass("active");
+  });
+
+  $("fieldset p").on('mouseenter', function() {
+    $this.addClass("active");
+  });
+  $("fieldset p").on('mouseleave', function() {
+    $this.removeClass("active");
+  });
+  
+  // Log hardware selection to the console
+  $("#btnHardware").on('click', function(e) {
+    e.preventDefault();
+    let $cpu = $('input[name="CPU"]:checked').attr('data-name');
+    let $ram = $('input[name="ram"]:checked').attr('data-name');
+    let $hardware = $('input[name="hardware"]:checked').attr('data-name');
+    console.log(`Selected CPU: ${$cpu}` + "\n" +
+                `Selected RAM: ${$ram}` + "\n" +
+                `Additional Hardware: ${$hardware}`);
+  });
+});
